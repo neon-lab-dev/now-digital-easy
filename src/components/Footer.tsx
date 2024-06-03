1441b6a9b3fbcb72b30362427a78b3efbc8b2807
Premanand Chowdhury <freudigprem@gmail.com>
2024-06-03 10:05:45 +0530

    chore: Updated footer hyper links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,98 +18,98 @@ import minus from "@/assets/icons/minus-svgrepo-com.svg";
 
 const FOOTER_LINKS = [
   {
-    title: "COMPANY",
+    title: 'COMPANY',
     links: [
       {
-        label: "About Us",
-        href: "#",
+        label: 'About Us',
+        href: '/about-us',
       },
       {
-        label: "Payment Method",
-        href: "#",
+        label: 'Payment Method',
+        href: '/payment-option',
       },
       {
-        label: "Refund Policy",
-        href: "#",
+        label: 'Refund Policy',
+        href: '/refund-policy',
       },
       {
-        label: "Contact Us",
-        href: "#",
+        label: 'Contact Us',
+        href: '/contact-us',
       },
     ],
   },
   {
-    title: "SERVICES",
+    title: 'SERVICES',
     links: [
       {
-        label: "Register Domains",
-        href: "#",
+        label: 'Register Domains',
+        href: '#',
       },
       {
-        label: "Transfer Domains",
-        href: "#",
+        label: 'Transfer Domains',
+        href: '#',
       },
       {
-        label: "Manage Domains",
-        href: "#",
+        label: 'Manage Domains',
+        href: '#',
       },
       {
-        label: "Web Hosting",
-        href: "#",
+        label: 'Web Hosting',
+        href: '#',
       },
       {
-        label: "Google Workspace",
-        href: "#",
+        label: 'Google Workspace',
+        href: '#',
       },
     ],
   },
   {
-    title: "PRODUCTS",
+    title: 'PRODUCTS',
     links: [
       {
-        label: "NDE Mail",
-        href: "#",
+        label: 'NDE Mail',
+        href: '#',
       },
       {
-        label: "Vision Now",
-        href: "#",
+        label: 'Vision Now',
+        href: '#',
       },
       {
-        label: "Peoples Now",
-        href: "#",
+        label: 'Peoples Now',
+        href: '#',
       },
       {
-        label: "Spot Now",
-        href: "#",
+        label: 'Spot Now',
+        href: '#',
       },
       {
-        label: "Marketing Studio",
-        href: "#",
+        label: 'Marketing Studio',
+        href: '#',
       },
     ],
   },
   {
-    title: "SUPPORT",
+    title: 'SUPPORT',
     links: [
       {
-        label: "Open Tickets",
-        href: "#",
+        label: 'Open Tickets',
+        href: '#',
       },
       {
-        label: "Blog",
-        href: "#",
+        label: 'Blog',
+        href: '#',
       },
       {
-        label: "Live Chat",
-        href: "#",
+        label: 'Live Chat',
+        href: '#',
       },
       {
-        label: "Change Management",
-        href: "#",
+        label: 'Change Management',
+        href: '#',
       },
       {
-        label: "Support",
-        href: "#",
+        label: 'Support',
+        href: '#',
       },
     ],
   },
@@ -122,7 +122,7 @@ const Footer = () => {
       <Image
         className="absolute inset-0 w-full h-full object-contain lg:object-cover z-0 object-top"
         src={footer}
-        alt={"footer"}
+        alt={'footer'}
       />
       <div className="relative z-10 wrapper mt-[150px] max-lg:mt-[100px] 3xl:mt-[200px] bg-gradient-footer max-md:mt-10">
         <div className="flex max-lg:flex-col justify-between max-lg:gap-10">
@@ -135,11 +135,10 @@ const Footer = () => {
                     onClick={() =>
                       setActiveLink(() => (i === activeLink ? null : i))
                     }
-                    className="focus:outline-none"
-                  >
+                    className="focus:outline-none">
                     <Image
                       src={activeLink === i ? minus : plus}
-                      alt={""}
+                      alt={''}
                       height={20}
                       width={20}
                       className="min-w-[20px] min-h-[20px] md:hidden"
@@ -174,32 +173,32 @@ const Footer = () => {
             </span>
             <div className="flex gap-8 py-2 px-2 items-center">
               <Link href="https://cloud.google.com/find-a-partner/partner/now-digital-easy">
-                <Image src={GooglePartner} alt={""} />
+                <Image src={GooglePartner} alt={''} />
               </Link>
               <Link href="https://www.facebook.com/nowdigitaleasyofficial/reviews">
-                <Image src={facebookReview} alt={""} />
+                <Image src={facebookReview} alt={''} />
               </Link>
               <Link href="https://www.google.com/search?q=now+digital+easy+&sca_esv=1408e98ab1991b60&ei=yZk8ZsXPHcGO4-EPssqWuAU&ved=0ahUKEwiF2OqMo4CGAxVBxzgGHTKlBVcQ4dUDCBA&uact=5&oq=now+digital+easy+&gs_lp=Egxnd3Mtd2l6LXNlcnAiEW5vdyBkaWdpdGFsIGVhc3kgMgUQABiABDIFEAAYgAQyBhAAGBYYHjIGEAAYFhgeMgsQABiABBiGAxiKBTILEAAYgAQYhgMYigVI9wVQ9wNY9wNwAXgAkAEAmAHtAaAB7QGqAQMyLTG4AQPIAQD4AQGYAgGgAvgBmAMAiAYBkgcDMi0xoAfyAw&sclient=gws-wiz-serp">
-                <Image src={GoogleReview} alt={""} className="rounded-[50%]" />
+                <Image src={GoogleReview} alt={''} className="rounded-[50%]" />
               </Link>
             </div>
           </div>
         </div>
         <div className="flex mt-[60px] py-2 items-center gap-16 max-md:gap-6">
-          <Image src={logo} alt={""} className="w-[80px]" />
+          <Image src={logo} alt={''} className="w-[80px]" />
           <div className="flex gap-3 items-center">
-            <Image src={india} alt={""} />
+            <Image src={india} alt={''} />
             <div className="flex gap-1">
               <span className="font-source-sans-pro text-[13px] text-[#151D8C] font-600">
-                - English{" "}
+                - English{' '}
               </span>
-              <Image src={vector} alt={""} />
+              <Image src={vector} alt={''} />
             </div>
             <div className="flex gap-1">
               <span className="font-source-sans-pro text-[13px] text-[#151D8C] font-600">
-                INR ₹{" "}
+                INR ₹{' '}
               </span>
-              <Image src={vector} alt={""} className="" />
+              <Image src={vector} alt={''} className="" />
             </div>
           </div>
         </div>
@@ -210,29 +209,29 @@ const Footer = () => {
           </span>
           <div className="flex gap-2 items-center justify-center">
             <span className="font-source-sans-pro text-[13px] max-md:text-[9px] text-[#151D8C]">
-              Privacy Policy
+              <Link href={'/privacy-policy'}>Privacy Policy</Link>
             </span>
             <span className="font-source-sans-pro text-[13px] max-md:text-[9px] text-[#151D8C]">
-              Terms and Conditions
+            <Link href={'/usage-terms'}>Terms and Conditions</Link>
             </span>
             {[
               {
-                href: "https://www.facebook.com/nowdigitaleasyofficial/",
+                href: 'https://www.facebook.com/nowdigitaleasyofficial/',
                 icon: facebook,
               },
               {
-                href: "https://www.instagram.com/nowdigitaleasy/",
+                href: 'https://www.instagram.com/nowdigitaleasy/',
                 icon: insta,
               },
               {
-                href: "https://in.linkedin.com/company/nowdigitaleasy",
+                href: 'https://in.linkedin.com/company/nowdigitaleasy',
                 icon: linkedin,
               },
             ].map((social, index) => (
               <Link key={index} href={social.href}>
                 <Image
                   src={social.icon}
-                  alt={""}
+                  alt={''}
                   className="w-[24px] h-[24px]"
                 />
               </Link>
