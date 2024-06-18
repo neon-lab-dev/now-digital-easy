8297559f5beecdff5592e5300198f01cc2b324e8
Premanand Chowdhury <freudigprem@gmail.com>
2024-06-18 08:57:54 +0530

    chore: hero title

--- src/app/(pages)/demo/_components/Hero.tsx
+++ src/app/(pages)/demo/_components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="wrapper absolute top-[71px] flex flex-row flex-wrap justify-center w-full gap-[80px] h-full">
       {/* Heading */}
-      <h1 className="2xl:max-w-[444px] heading1 mt-[50px] text-primary-500 text-center lg:text-left">
+      <h1 className="2xl:max-w-[444px] text-heading1 leading-heading1 font-[900] mt-[50px] text-primary-500 text-center lg:text-left">
         Book a demo to explore NDE
       </h1>
       <Form />
