2a0af0299e6334cccb5ffebe52bc1a343c06a8f3
jrtilak <iamjrtilak@gmail.com>
2024-06-24 13:18:13 +0545

    refactor: Update CartItemForLocalData and CartItemForRemoteData components for responsive layout using flexbox

--- src/app/(pages)/googleworkspace/_components/DomainCheckout.tsx
+++ src/app/(pages)/googleworkspace/_components/DomainCheckout.tsx
@@ -127,7 +127,7 @@ const DomainCheckout = ({ isOpen, setIsOpen, selectedService }: Props) => {
       style={{
         scale: isOpen.open ? 1 : 0,
       }}
-      className="bg-gradient-checkout transition-all w-[1000px]  border border-[#000659] shadow-[#00065980] shadow-2xl rounded-xl fixed bottom-6 left-1/2 -translate-x-1/2 z-[100]"
+      className="bg-gradient-checkout transition-all w-[90%] max-w-[1020px]  border border-[#000659] shadow-[#00065980] shadow-2xl rounded-xl fixed bottom-6 left-1/2 -translate-x-1/2 z-[100]"
     >
       <button
         onClick={() => {
@@ -138,89 +138,100 @@ const DomainCheckout = ({ isOpen, setIsOpen, selectedService }: Props) => {
         <Image src={x} alt="" className="h-6 w-6" />
       </button>
       {!isBuyNowClicked ? (
-        <div className="flex justify-between items-center px-6  py-6">
-          <div className="flex flex-col gap-2">
+        <div className="flex flex-col p-4 lg:px-6 lg:py-8 gap-4 lg:flex-row lg:gap-20 lg:items-center m-auto justify-center">
+          <div className="flex flex-col">
             <span className="text-[17px] text-[#000659] leading-[15px]">
               Plan Name
             </span>
             <span className="font-400 text-[15px]">{isOpen.title}</span>
           </div>
-          <div className="flex flex-col ">
-            <label htmlFor="" className="text-[17px] text-[#000659]">
-              No of Accounts
-            </label>
-            <input
-              type="number"
-              value={selectedNumberOfAccounts}
-              onChange={(e) => {
-                if (Number(e.target.value) > 300) {
-                  toast.error("Maximum number of accounts is 300");
-                  setSelectedNumberOfAccounts(300);
-                  return;
-                }
-                setSelectedNumberOfAccounts(Number(e.target.value));
-              }}
-              min={1}
-              defaultValue={1}
-              max={300}
-              className="placeholder:text-center text-center text-[#646464] bg-transparent placeholder:text-[#646464] border border-[#646464] p-1 w-[100px] rounded-lg"
-            />
+          <div className="flex flex-col gap-4 md:flex-row justify-between lg:justify-normal lg:gap-20">
+            <div className="flex gap-2 justify-between xs:justify-normal xs:gap-9 sm:gap-12 items-center">
+              <div className="flex flex-col ">
+                <label
+                  htmlFor=""
+                  className="text-sm xs:text-base md:text-lg text-[#000659]"
+                >
+                  No of Accounts
+                </label>
+                <input
+                  type="number"
+                  value={selectedNumberOfAccounts}
+                  onChange={(e) => {
+                    if (Number(e.target.value) > 300) {
+                      toast.error("Maximum number of accounts is 300");
+                      setSelectedNumberOfAccounts(300);
+                      return;
+                    }
+                    setSelectedNumberOfAccounts(Number(e.target.value));
+                  }}
+                  min={1}
+                  defaultValue={1}
+                  max={300}
+                  className="placeholder:text-center text-center text-[#646464] bg-transparent placeholder:text-[#646464] border border-[#646464] p-1 w-auto rounded-lg"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className="text-sm xs:text-base md:text-lg text-[#000659] leading-[15px]">
+                  Price
+                </span>
+                <span className="">
+                  {getSelectedCurrencySymbol(currency?.code!)}
+                  {selectedPricing?.offerPrice ??
+                    selectedService?.price[0].offerPrice}
+                </span>
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-sm xs:text-base md:text-lg text-[#000659]">
+                  Duration
+                </span>
+                <select
+                  className="border border-[#646464] p-1 w-[100px] xs:w-auto rounded-lg"
+                  value={selectedPricing?.period}
+                  onChange={(e) => {
+                    const selectedPrice = selectedService?.price.find(
+                      (price) => price.period === e.target.value
+                    );
+                    setSelectedPricing(selectedPrice!);
+                  }}
+                >
+                  {selectedService?.price.map((price, index) => (
+                    <option key={index} value={price.period}>
+                      {price.period}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className="flex justify-between items-center gap-6">
+              <div className="flex flex-col ">
+                <span className="text-sm xs:text-base md:text-lg text-[#000659] leading-[15px]">
+                  Total
+                </span>
+                <span className="text-base lg:text-2xl !font-source-sans-pro">
+                  {getSelectedCurrencySymbol(currency?.code!)}
+                  {(
+                    (selectedPricing?.offerPrice ??
+                      selectedService?.price[0].offerPrice) *
+                    selectedNumberOfAccounts
+                  ).toFixed(2)}
+                </span>
+              </div>
+              <button
+                onClick={() => {
+                  setIsBuyNowClicked(true);
+                }}
+                disabled={selectedNumberOfAccounts < 1}
+                className="px-5 xs:px-5 lg:min-w-24 sm:px-4 lg:py-4 py-2 rounded-xl bg-[#0009FF] text-white disabled:opacity-75"
+              >
+                Buy Now
+              </button>
+            </div>
           </div>
-          <div className="flex flex-col gap-2">
-            <span className="text-[17px] text-[#000659] leading-[15px]">
-              Price
-            </span>
-            <span>
-              {getSelectedCurrencySymbol(currency?.code!)}
-              {selectedPricing?.offerPrice ??
-                selectedService?.price[0].offerPrice}
-            </span>
-          </div>
-          <div className="flex flex-col h-[100px] pt-5 gap-1">
-            <span className="text-[17px] text-[#000659]">Duration</span>
-            <select
-              className="border border-[#646464] p-1 w-[100px] rounded-lg"
-              value={selectedPricing?.period}
-              onChange={(e) => {
-                const selectedPrice = selectedService?.price.find(
-                  (price) => price.period === e.target.value
-                );
-                setSelectedPricing(selectedPrice!);
-              }}
-            >
-              {selectedService?.price.map((price, index) => (
-                <option key={index} value={price.period}>
-                  {price.period}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="flex flex-col ">
-            <span className="text-[17px] text-[#000659] leading-[15px]">
-              Total
-            </span>
-            <span className="text-[24px] font-600">
-              {getSelectedCurrencySymbol(currency?.code!)}
-              {(
-                (selectedPricing?.offerPrice ??
-                  selectedService?.price[0].offerPrice) *
-                selectedNumberOfAccounts
-              ).toFixed(2)}
-            </span>
-          </div>
-          <button
-            onClick={() => {
-              setIsBuyNowClicked(true);
-            }}
-            disabled={selectedNumberOfAccounts < 1}
-            className="px-8 py-4 rounded-xl bg-[#0009FF] text-white disabled:opacity-75"
-          >
-            Buy Now
-          </button>
         </div>
       ) : (
-        <div className="flex flex-col items-center py-6">
-          <div className="flex justify-start items-start w-[900px]">
+        <div className="flex flex-col items-center p-6 gap-4">
+          <div className="flex justify-start flex-col md:flex-row items-start max-w-[900px] gap-1 md:gap-6 w-full">
             {[
               {
                 option: "Register a New Domain",
@@ -234,7 +245,7 @@ const DomainCheckout = ({ isOpen, setIsOpen, selectedService }: Props) => {
               <label
                 htmlFor={option.value}
                 key={index}
-                className="flex items-center ps-4 rounded cursor-pointer"
+                className="flex items-center rounded cursor-pointer"
               >
                 <input
                   id={option.value}
@@ -245,7 +256,7 @@ const DomainCheckout = ({ isOpen, setIsOpen, selectedService }: Props) => {
                   onChange={(e) => setRadioInputValue(e.target.value)}
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 "
                 />
-                <span className="w-full py-4 ms-2 font-medium text-black">
+                <span className="w-full font-medium text-black ml-4">
                   {option.option}
                 </span>
               </label>
@@ -265,7 +276,7 @@ const DomainCheckout = ({ isOpen, setIsOpen, selectedService }: Props) => {
                 });
               }
             }}
-            className="flex justify-center"
+            className="flex justify-center w-full text-sm md:text-base"
           >
             <input
               type="text"
@@ -274,12 +285,12 @@ const DomainCheckout = ({ isOpen, setIsOpen, selectedService }: Props) => {
                 setInputValue(e.target.value);
               }}
               autoFocus
-              className="bg-transparent border border-black w-[700px] h-[50px] px-4 "
+              className="bg-transparent flex-grow border border-black max-w-[700px] w-full  px-4 "
             />
             {radioInputValue === "register" ? (
               <button
                 disabled={isCheckAvailabilityPending || !inputValue}
-                className="px-4 py-2 h-[51px] bg-[#0009FF] disabled:opacity-90 text-white  shadow-black shadow-md"
+                className="px-4 py-2 min-w-fit bg-[#0009FF] disabled:opacity-90 text-white  shadow-black shadow-md"
               >
                 {isCheckAvailabilityPending ? (
                   <BeatLoader color="#ffffff" size={12} />
@@ -345,7 +356,7 @@ const DomainCheckout = ({ isOpen, setIsOpen, selectedService }: Props) => {
                     closeModals();
                   }
                 }}
-                className="px-7 py-2 h-[50px] bg-[#0009FF] text-white shadow-black shadow-md"
+                className="px-7 py-2 bg-[#0009FF] text-white shadow-black shadow-md min-w-fit"
               >
                 {isAddToCartPending ? (
                   <BeatLoader color="#ffffff" size={12} />
@@ -358,7 +369,7 @@ const DomainCheckout = ({ isOpen, setIsOpen, selectedService }: Props) => {
           {isCheckAvailabilitySuccess &&
             radioInputValue === "register" &&
             domainAvailabilityData && (
-              <div className="w-full px-20 mt-4">
+              <div className="w-full lg:px-20 mt-4">
                 <div></div>
                 {isCheckAvailabilityPending ? (
                   <div className=" items-start mt-2 leading-10 px-4 mr-[270px]">
@@ -373,10 +384,11 @@ const DomainCheckout = ({ isOpen, setIsOpen, selectedService }: Props) => {
                             <span>Domain</span>
                           </div>
                           <div className="flex items-center gap-5">
-                            <div className="min-w-28 text-left">Year</div>
-                            <div className="min-w-24">Pricing</div>
-
-                            <div className="min-w-28"></div>
+                            <div className="sm:min-w-28 flex-grow sm:flex-grow-0 text-left">
+                              Year
+                            </div>
+                            <div className="sm:min-w-24">Pricing</div>
+                            <div className="sm:min-w-[160px]"></div>
                           </div>
                         </div>
                         <hr className=" bg-[#64646480]  h-[2px]" />
@@ -477,8 +489,8 @@ const DomainCard = ({
 
   return (
     <div>
-      <div className="flex justify-between items-center gap-10 my-4">
-        <div className="flex gap-4 items-center">
+      <div className="flex justify-between sm:items-center gap-10 my-4">
+        <div className="flex gap-2 xs:gap-4 sm:items-center">
           <span>{domain}</span>
           {status !== "available" && (
             <>
@@ -486,11 +498,11 @@ const DomainCard = ({
             </>
           )}
         </div>
-        <div className="flex items-center gap-5">
-          {status === "available" && (
-            <>
+        {status === "available" && (
+          <div className="flex flex-col sm:flex-row sm:items-center gap-2  sm:gap-9 min-w-[168px]">
+            <div className="flex items-center gap-9">
               <select
-                className="border border-[#646464] p-1 w-[100px] rounded-lg"
+                className="border border-[#646464] p-1 sm:w-[100px] rounded-lg"
                 value={selectedPricing?._id}
                 onChange={(e) => {
                   setSelectedPricing(
@@ -504,52 +516,52 @@ const DomainCard = ({
                   </option>
                 ))}
               </select>
-              <div className="min-w-24">
+              <div className="sm:min-w-24">
                 {getSelectedCurrencySymbol(currency?.code!)}
                 {selectedPricing?.registerPrice}
               </div>
-            </>
-          )}
+            </div>
 
-          <button
-            disabled={status !== "available" || isAddedToCart}
-            onClick={() => {
-              const token = authToken;
-              const data = {
-                product: "gsuite",
-                productId: productId,
-                domainName: domain,
-                period,
-                type: "new",
-                qty,
-              } as const;
+            <button
+              disabled={status !== "available" || isAddedToCart}
+              onClick={() => {
+                const token = authToken;
+                const data = {
+                  product: "gsuite",
+                  productId: productId,
+                  domainName: domain,
+                  period,
+                  type: "new",
+                  qty,
+                } as const;
 
-              if (token) {
-                handleAddToCart(data);
-              } else {
-                dispatch(
-                  // @ts-ignore
-                  addCartItem({
-                    ...data,
-                    name,
-                  })
-                );
-                toast.success("Domain added to cart");
-                dispatch(setIsSidebarOpen(!isSidebarOpen));
-                dispatch(setIsSideBarActive(true));
-                setIsOpen(false);
-              }
-            }}
-            className=" bg-[#0009FF] text-white rounded-[5px] p-2 shadow-black shadow-md disabled:opacity-50 min-w-28"
-          >
-            {isAddedToCart
-              ? "Added to cart"
-              : isAddToCartPending
-              ? "Adding ..."
-              : "Add to cart"}
-          </button>
-          <hr className=" bg-[#64646480]  h-[2px]" />
-        </div>
+                if (token) {
+                  handleAddToCart(data);
+                } else {
+                  dispatch(
+                    // @ts-ignore
+                    addCartItem({
+                      ...data,
+                      name,
+                    })
+                  );
+                  toast.success("Domain added to cart");
+                  dispatch(setIsSidebarOpen(!isSidebarOpen));
+                  dispatch(setIsSideBarActive(true));
+                  setIsOpen(false);
+                }
+              }}
+              className=" bg-[#0009FF] text-white rounded-[5px] p-2 shadow-black shadow-md disabled:opacity-50 sm:min-w-28"
+            >
+              {isAddedToCart
+                ? "Added to cart"
+                : isAddToCartPending
+                ? "Adding ..."
+                : "Add to cart"}
+            </button>
+            <hr className=" bg-[#64646480]  h-[2px]" />
+          </div>
+        )}
       </div>
       <hr className=" bg-[#64646480]  h-[2px]" />
     </div>
